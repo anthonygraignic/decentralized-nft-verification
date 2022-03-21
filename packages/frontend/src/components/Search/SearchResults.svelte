14a4a839c9ede7197b0abf88b9eedb578cef637d
Anthony Graignic <anthony@graignic.com>
2022-03-21 21:59:30 +0100

    Add card skeleton loading

--- packages/frontend/src/components/Search/SearchResults.svelte
+++ packages/frontend/src/components/Search/SearchResults.svelte
@@ -6,32 +6,19 @@
 	export let loading = false;
 </script>
 
-{#if loading}
-	<div class="flex items-center justify-center mx-4 my-2">
-		<svg
-			class="animate-spin -ml-1 mr-3 h-5 w-5 text-white dark:text-black"
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-		>
-			<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-			<path
-				class="opacity-75"
-				fill="currentColor"
-				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-			/>
-		</svg>
-		<p>Searching...</p>
-	</div>
-{:else if results}
-	<!--  -->
-	{#if results.length > 0}
-		<div class="grid grid-flow-row sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4 my-4" in:fade>
+<div class="grid grid-flow-row sm:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4 my-4" in:fade>
+	{#if loading}
+		{#each Array.from(Array(6).keys()) as item}
+			<CollectionCard collection={{ id: '', status: '' }} loading={true} />
+		{/each}
+	{:else if results}
+		<!--  -->
+		{#if results.length > 0}
 			{#each results as collection}
 				<CollectionCard {collection} />
 			{/each}
-		</div>
-	{:else}
-		<p in:fade>No results</p>
+		{:else}
+			<p in:fade class="sm:col-span-2 lg:col-span-3">No results</p>
+		{/if}
 	{/if}
-{/if}
+</div>
