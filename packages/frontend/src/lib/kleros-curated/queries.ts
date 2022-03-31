15ad672c4dc4ee8ea1f11c8b9b40320e0498c4b8
Anthony Graignic <anthony@graignic.com>
2022-03-31 14:55:01 +0200

    Update GraphQL queries to use Mizu's subgraph

--- packages/frontend/src/lib/kleros-curated/queries.ts
+++ packages/frontend/src/lib/kleros-curated/queries.ts
@@ -1,13 +1,21 @@
 import { gql } from '@apollo/client/core';
 
-
 // Cannot use itemSearch() as it needs an exact match as text
 // Using keyword search rather for simplicity's sake
 
+/**
+ * Search collections by name using the `contain` function in a case insensitive way.
+ */
 export const COLLECTIONS = gql`
-	query searchCollections($search: String) {
+	query searchbyName($search: String) {
 		litems(
-			where: { registry_in: ["0x2f19f817bbf800b487b7f2e51f24ad5ea0222463","0xd5994f15be9987104d9821aa99d1c97227c7c08c"], keywords_contains: $search }
+			where: {
+				registry_in: [
+					"0x2f19f817bbf800b487b7f2e51f24ad5ea0222463"
+					"0xd5994f15be9987104d9821aa99d1c97227c7c08c"
+				]
+				key0_contains_nocase: $search
+			}
 		) {
 			id
 			itemID
@@ -29,15 +37,28 @@ export const COLLECTIONS = gql`
 				requester
 				requestType
 			}
+			key0
+			key1
+			key2
+			key3
+			key4
 		}
 	}
 `;
-// TODO Use where condition in LItems which won't search across all Kleros curated list
-// But keywords is case sensitive so use itemSearch seems a better option for now
-// Could concat text with `0x2f19f817bbf800b487b7f2e51f24ad5ea0222463 & $contractAddress` as in TCR
+/**
+ * Search collections by contract address in a case insensitive way
+ */
 export const COLLECTIONS_BY_CONTRACT_ADDRESS = gql`
 	query searchByContractAddress($contractAddress: String) {
-		itemSearch(text: $contractAddress) {
+		litems(
+			where: {
+				registry_in: [
+					"0x2f19f817bbf800b487b7f2e51f24ad5ea0222463"
+					"0xd5994f15be9987104d9821aa99d1c97227c7c08c"
+				]
+				key2_contains_nocase: $contractAddress
+			}
+		) {
 			id
 			itemID
 			data
@@ -58,13 +79,16 @@ export const COLLECTIONS_BY_CONTRACT_ADDRESS = gql`
 				requester
 				requestType
 			}
+			key0
+			key1
+			key2
+			key3
+			key4
 		}
 	}
 `;
 
 // collection -> requests -> rounds -> evidences JSON files
-// Need to get evidences files from on-chain events 
-// see https://github.com/kleros/gtcr/blob/master/src/pages/light-item-details/index.js#L246
 export const GET_COLLECTION = gql`
 	query getCollection($id: String) {
 		litem(id: $id) {
@@ -77,47 +101,53 @@ export const GET_COLLECTION = gql`
 			data
 			registryAddress
 			props {
-				id
-				label
-				value
+			  id
+			  label
+			  value
 			}
 			requests(orderBy: submissionTime, orderDirection: desc) {
+			  id
+			  submissionTime
+			  resolved
+			  challenger
+			  requester
+			  requestType
+			  disputed
+			  disputeID
+			  arbitrator
+			  arbitratorExtraData
+			  disputeOutcome
+			  rounds {
 				id
-				submissionTime
-				resolved
-				challenger
-				requester
-				requestType
-				disputed
-				disputeID
-				arbitrator
-				arbitratorExtraData
-				disputeOutcome
-				rounds {
-					id
-					rulingTime
-					ruling
-					creationTime
-					contributions {
-						id
-						contributor
-						withdrawable
-						side
-					}
-					numberOfContributions
-					amountPaidRequester
-					amountPaidChallenger
-					hasPaidRequester
-					hasPaidChallenger
-					feeRewards
-					appealPeriodStart
-					appealPeriodEnd
-					appealed
+				rulingTime
+				ruling
+				creationTime
+				contributions {
+				  id
+				  contributor
+				  withdrawable
+				  side
 				}
-				metaEvidence {
-					id
-					URI
-				}
+				numberOfContributions
+				amountPaidRequester
+				amountPaidChallenger
+				hasPaidRequester
+				hasPaidChallenger
+				feeRewards
+				appealPeriodStart
+				appealPeriodEnd
+				appealed
+			  }
+			  evidences {
+				  evidenceGroupID
+				number
+				party
+				URI
+			  }
+			  metaEvidence {
+				id
+				URI
+			  }
 			}
 			disputed
 			latestChallenger
